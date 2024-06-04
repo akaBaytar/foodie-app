5d5d8fc497c1db05c23cecc563e9733c16c9b029
Burak Bilgili <burak.bilgili@icloud.com>
2024-06-04 19:01:38 +0300

    landing page structure

--- app/page.js
+++ app/page.js
@@ -1,3 +1,41 @@
+import Link from 'next/link';
+import classes from './page.module.css';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}></div>
+        <div className={classes.container}>
+          <div className={classes.hero}>
+            <h1>Next level food for next level foodies</h1>
+            <p>Taste & share food from all over the world</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href='/community'>Join the Community</Link>
+            <Link href='meals'>Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            Foodies is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+        </section>
+
+        <section className={classes.section}>
+          <h2>Why Foodies?</h2>
+          <p>
+            Foodies is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
+  );
 }
