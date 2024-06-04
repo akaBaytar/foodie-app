74852bb2aaba05af38e113984fdb93432b813bc6
Burak Bilgili <burak.bilgili@icloud.com>
2024-06-04 16:39:22 +0300

    add routes, logo and favicon

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Foodie
-      </h1>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>Foodie</h1>
     </main>
   );
 }
