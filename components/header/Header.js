5074ccdda907ca1487b0dc200e9fe0226b017fea
Burak Bilgili <burak.bilgili@icloud.com>
2024-06-04 17:36:09 +0300

    refactor background svg as component

---
+++ components/header/Header.js
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import Logo from '@/public/assets/logo.png';
+import Background from './Background';
+
+import classes from './Header.module.css';
+
+export default function MainHeader() {
+  return (
+    <>
+      <Background />
+      <header className={classes.header}>
+        <Link href='/' className={classes.logo}>
+          <Image
+            src={Logo.src}
+            alt='Foodie'
+            width='1024'
+            height='1024'
+            priority
+          />
+          <span>Foodie</span>
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href='/meals'>Meals</Link>
+            </li>
+            <li>
+              <Link href='/community'>Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
