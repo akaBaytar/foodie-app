61421b98bda81189d22bbcd352a9d1d4adaf5283
Burak Bilgili <burak.bilgili@icloud.com>
2024-06-04 17:25:33 +0300

    add header and styles

--- app/page.js
+++ app/page.js
@@ -1,9 +1,3 @@
-import Link from 'next/link';
-
 export default function Home() {
-  return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>Foodie</h1>
-    </main>
-  );
+  return <main></main>;
 }
